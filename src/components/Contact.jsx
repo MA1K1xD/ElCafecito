7f2971f6ab4daa68ff69fe31e9a5970126d59eed
mikoIOS <miguel.espinoza.a@utec.edu.pe>
2026-07-14 17:25:30 -0500

    traces

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,21 +6,23 @@ export default function Contact() {
         <div className="contact-grid">
           <div className="contact-info">
             <div className="info-item">
-              <span className="info-icon">📍</span>
+              <svg className="info-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+                <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z"/>
+              </svg>
               <div>
                 <h3>Dirección</h3>
                 <p>Calle Principal #123, Centro</p>
               </div>
             </div>
             <div className="info-item">
-              <span className="info-icon">📞</span>
+              <img src="public/images/phone.svg" alt="Teléfono" className="info-icon" />
               <div>
                 <h3>Teléfono</h3>
                 <p>+52 (555) 123-4567</p>
               </div>
             </div>
             <div className="info-item">
-              <span className="info-icon">🕐</span>
+              <img src="public/images/clock-arrow-right.svg" alt="Horario" className="info-icon" />
               <div>
                 <h3>Horario</h3>
                 <p>Lun - Vie: 7:00 AM - 9:00 PM<br />Sáb - Dom: 8:00 AM - 10:00 PM</p>
